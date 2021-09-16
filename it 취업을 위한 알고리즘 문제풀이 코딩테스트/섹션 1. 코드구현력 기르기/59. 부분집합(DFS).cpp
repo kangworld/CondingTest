704d1c82a6d427d054c74877484530a8566e8825
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-16 21:44:59 +0900

    Complete
    59. 부분집합(DFS)
    
    60. 합이 같은 부분집합
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/59. 부분집합(DFS).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool a[11];
+int n;
+
+void DFS(int level)
+{
+	if (level > n)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (a[i])
+				cout << i << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	a[level] = true;
+	DFS(level + 1);
+	a[level] = false;
+	DFS(level + 1);
+}
+
+int main()
+{
+	cin >> n;
+
+	DFS(1);
+}
---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/60. 합이 같은 부분집합.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int b[11];
+int total = 0;
+bool isEqual = false;
+int n;
+
+void DFS(int level, int sum)
+{
+	if (isEqual) return;
+
+	if (level == n + 1)
+	{
+		if (total - sum == sum)
+			isEqual = true;
+
+		return;
+	}
+
+	
+	DFS(level + 1, sum + b[level + 1]);
+	DFS(level + 1, sum);
+}
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> b[i];
+		total += b[i];
+	}
+
+	DFS(1, 0);
+
+	if (isEqual)
+		cout << "YES";
+	else
+		cout << "NO";
+
+	return 0;
+}
