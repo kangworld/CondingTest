0b5fe8ffdba7369ed20dda68acdde20b8a318ceb
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-17 15:45:00 +0900

    Complete
    61. 특정 수 만들기
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/61. 특정 수 만들기.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int a[11];
+int n, m, res = 0;
+
+void DFS(int level, int sum)
+{
+	if (level == n + 1)
+	{
+		if (sum == m) res++;
+		return;
+	}
+
+	DFS(level + 1, sum + a[level]);
+	DFS(level + 1, sum - a[level]);
+	DFS(level + 1, sum);
+}
+
+int main()
+{
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+
+	DFS(1, 0);
+
+	if (res == 0)
+		cout << "-1";
+	else
+		cout << res;
+}
