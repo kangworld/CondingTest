95e8433d1e4b3bdccb54542af0bdce0a096771cf
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-17 23:54:49 +0900

    Complete
    62. 병합정렬
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/62. 병합정렬.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[101];
+int tmp[101];
+
+void divide(int lt, int rt)
+{
+	int mid;
+
+	if (lt < rt)
+	{
+		mid = (lt + rt) / 2;
+		divide(lt, mid);
+		divide(mid + 1, rt);
+
+		int p1 = lt;
+		int p2 = mid + 1;
+		int p3 = lt;
+		while (p1 <= mid && p2 <= rt)
+		{
+			if (arr[p1] < arr[p2]) tmp[p3++] = arr[p1++];
+			else tmp[p3++] = arr[p2++];
+		}
+
+		while (p1 <= mid) tmp[p3++] = arr[p1++];
+		while (p2 <= rt) tmp[p3++] = arr[p2++];
+
+		for (int i = lt; i <= rt; i++)
+			arr[i] = tmp[i];
+	}
+}
+
+int main()
+{
+	int n; 
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+
+	divide(1, n);
+
+	for (int i = 1; i <= n; i++)
+		cout << tmp[i] << " ";
+}
