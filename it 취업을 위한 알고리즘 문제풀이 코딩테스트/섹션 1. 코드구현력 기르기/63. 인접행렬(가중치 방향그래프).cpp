0c7de453e1736efacfc030e1433ecb971da36560
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-18 22:37:04 +0900

    Complete
    63. 인접행렬(가중치 방향그래프)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/63. 인접행렬(가중치 방향그래프).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, m, e1, e2, cost;
+
+	cin >> n >> m;
+
+	vector<vector<int>> vec(n + 1, vector<int>(n + 1, 0));
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> e1 >> e2 >> cost;
+
+		vec[e1][e2] = cost;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+			cout << vec[i][j] << " ";
+		cout << endl;
+	}
+}
