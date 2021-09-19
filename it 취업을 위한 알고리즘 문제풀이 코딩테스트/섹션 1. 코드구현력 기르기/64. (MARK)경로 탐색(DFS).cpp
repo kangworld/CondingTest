3ca5468acfeb4744b6f45e0580dfe96d00d9696e
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-19 21:52:59 +0900

    Complete
    64. (MARK)경로 탐색(DFS)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/64. (MARK)경로 탐색(DFS).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, e1, e2, res = 0;
+bool visited[21];
+int arr[21][21];
+
+void DFS(int now)
+{
+	if (now == n)
+	{
+		res++;
+		return;
+	}
+
+	for (int next = 1; next <= n; next++)
+	{
+		if (visited[next])
+			continue;
+
+		if (arr[now][next] != 1)
+			continue;
+
+		visited[next] = true;
+		DFS(next);
+		visited[next] = false;
+	}
+	
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> e1 >> e2;
+		arr[e1][e2] = 1;
+	}
+
+	visited[1] = true;
+	DFS(1);
+
+	cout << res;
+}
