b61c7887e401911e74ea0b73fca4481492021784
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-20 23:21:48 +0900

    Complete
    65. 미로탐색(DFS)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/65. 미로탐색(DFS).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int map[8][8];
+bool visited[8][8];
+int dirY[4] = { -1, 1, 0 , 0 };
+int dirX[4] = { 0, 0, -1, 1 };
+int res = 0;
+
+void DFS(int y, int x)
+{
+	if (y == 7 && x == 7)
+	{
+		res++;
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		int nextY = y + dirY[i];
+		int nextX = x + dirX[i];
+
+		if (nextY < 1 || nextY > 7 || nextX < 1 || nextX > 7)
+			continue;
+
+		if (map[nextY][nextX] || visited[nextY][nextX])
+			continue;
+
+		visited[nextY][nextX] = true;
+		DFS(nextY, nextX);
+		visited[nextY][nextX] = false;
+	}
+}
+
+int main()
+{
+	for (int i = 1; i <= 7; i++)
+		for (int j = 1; j <= 7; j++)
+			cin >> map[i][j];
+
+	visited[1][1] = true;
+	DFS(1, 1);
+
+	cout << res;
+}
