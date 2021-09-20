02352908bcb7bd4dbd2e8506574f6f7c55a24a05
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-20 23:22:03 +0900

    Complete
    66. 경로 탐색(DFS 인접리스트 방법)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/66. 경로 탐색(DFS 인접리스트 방법).cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, e1, e2, res = 0;
+vector<int> vec[21];
+bool visited[21];
+
+void DFS(int now)
+{
+	if (now == n)
+	{
+		res++;
+		return;
+	}
+
+	for (int next : vec[now])
+	{
+		if (visited[next])
+			continue;
+
+		visited[next] = true;
+		DFS(next);
+		visited[next] = false;
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	
+	for (int i = 0; i < m; i++)
+	{
+		cin >> e1 >> e2;
+
+		vec[e1].push_back(e2);
+	}
+
+	visited[1] = true;
+	DFS(1);
+
+	cout << res;
+}
