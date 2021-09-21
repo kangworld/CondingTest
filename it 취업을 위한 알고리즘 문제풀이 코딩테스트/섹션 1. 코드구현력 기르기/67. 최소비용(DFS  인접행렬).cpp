99eaa7dbfe028826d5c3950c09439d27f9eeb462
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-21 23:58:48 +0900

    Complete
    67. 최소비용(DFS  인접행렬)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/67. 최소비용(DFS  인접행렬).cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, nd1, nd2, cost, res = 99999;
+int arr[21][21];
+bool visited[21];
+
+void DFS(int now, int sum)
+{
+	if (now == n)
+	{
+		res = sum < res ? sum : res;
+		return;
+	}
+
+	for (int next = 1; next <= n; next++)
+	{
+		if (!arr[now][next])
+			continue;
+
+		if (visited[next])
+			continue;
+
+		visited[next] = true;
+		DFS(next, sum + arr[now][next]);
+		visited[next] = false;
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> nd1 >> nd2 >> cost;
+		arr[nd1][nd2] = cost;
+	}
+
+	visited[1] = true;
+	DFS(1, 0);
+
+	cout << res;
+}
