80b117e091945a5fb401e193824e500b9448b758
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-22 19:22:45 +0900

    Complete
    70. 그래프 최단거리(BFS)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/70. 그래프 최단거리(BFS).cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int visited[21];
+int dist[21];
+vector<int> vec[21];
+int n, m, nd1, nd2;
+queue<int> q;
+
+
+void BFS(int start)
+{
+	visited[start] = true;
+	dist[start] = 0;
+	q.push(start);
+
+	while (q.size() > 0)
+	{
+		int now = q.front();
+		q.pop();
+
+		for (int next : vec[now])
+		{
+			if (visited[next])
+				continue;
+
+			visited[next] = true;
+			q.push(next);
+
+			dist[next] = dist[now] + 1;
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> nd1 >> nd2;
+		vec[nd1].push_back(nd2);
+	}
+
+	BFS(1);
+
+	for (int i = 2; i <= n; i++)
+		cout << i << " : " << dist[i] << endl;
+}
