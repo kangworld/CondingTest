dce7f4cada21e747cf12cc99aabfc6cb07cf01f7
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-22 19:20:25 +0900

    Complete
    68. 최소비용(DFS  가중치 방향그래프 인접리스트)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/68. 최소비용(DFS  가중치 방향그래프 인접리스트).cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int n, m, n1, n2, cost, res = 9999;
+
+//first : node 
+//second : cost
+vector<vector<pair<int, int>>> vec(21);
+int visited[21];
+
+
+void DFS(int now, int sum)
+{
+	if (now == n)
+	{
+		res = sum < res ? sum : res;
+		return;
+	}
+
+	for (pair<int, int> p : vec[now])
+	{
+		if (visited[p.first])
+			continue;
+
+		visited[p.first] = true;
+		DFS(p.first, sum + p.second);
+		visited[p.first] = false;
+	}
+
+}
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> n1 >> n2 >> cost;
+
+		vec[n1].push_back(make_pair(n2, cost));
+	}
+
+	visited[1] = true;
+	DFS(1, 0);
+
+	cout << res;
+}
