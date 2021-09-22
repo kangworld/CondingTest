31c964e055251b22c55bf87582ec0d69cd85dc0a
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-22 19:20:46 +0900

    Complete
    69. 이진트리 넓이우선탐색(BFS)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/69. 이진트리 넓이우선탐색(BFS).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int visited[7];
+vector<vector<int>> vec(7);
+
+void BFS(int start)
+{
+	queue<int> q;
+
+	q.push(start);
+	visited[start] = true;
+
+	while (q.size() > 0)
+	{
+		int now = q.front();
+		q.pop();
+		cout << now << " ";
+
+		for (int next : vec[now])
+		{
+			if (visited[next])
+				continue;
+
+			q.push(next);
+		}
+	}
+}
+
+int main()
+{
+	freopen("input.txt", "rt", stdin);
+
+	int s, e;
+
+	for (int i = 1; i <= 6; i++)
+	{
+		cin >> s >> e;
+		vec[s].push_back(e);
+	}
+
+	BFS(1);
+}
