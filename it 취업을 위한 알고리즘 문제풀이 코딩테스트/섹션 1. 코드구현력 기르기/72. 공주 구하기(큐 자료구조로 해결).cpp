a075cf4c3dc1d75b8e787492a8a08d347956f14c
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-25 19:59:19 +0900

    Complete
    72. 공주 구하기(큐 자료구조로 해결)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/72. 공주 구하기(큐 자료구조로 해결).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main(void)
+{
+	int n, k, now, count = 0;
+	queue<int> q;
+
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	while (q.size() > 0)
+	{
+		now = q.front();
+		q.pop();
+
+		count++;
+
+		if (count == k)
+			count = 0;
+		else
+			q.push(now);
+	}
+
+	cout << now;
+}
