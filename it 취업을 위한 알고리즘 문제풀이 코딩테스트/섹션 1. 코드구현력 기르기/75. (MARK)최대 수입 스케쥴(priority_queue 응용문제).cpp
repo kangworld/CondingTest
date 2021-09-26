d6b2bf2e91883b06df4bf91e97c8f8a938c3f8c1
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-09-26 22:16:55 +0900

    Complete
    75. (MARK)최대 수입 스케쥴(priority_queue 응용문제)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/75. (MARK)최대 수입 스케쥴(priority_queue 응용문제).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	return a.second > b.second;
+}
+
+int main(void)
+{
+	int n, m, d, max, pos = 0, res = 0;
+
+	priority_queue<int> pq;
+	vector<pair<int, int>> v;
+
+ 	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m >> d;
+
+		v.push_back(make_pair(m, d));
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	max = v[0].second;
+	for (int day = max; day > 0; day--)
+	{
+		for ( ; pos < n; pos++)
+		{
+			if (v[pos].second != day) break;
+			pq.push(v[pos].first);
+		}
+
+		if (!pq.empty())
+		{
+			res += pq.top();
+			pq.pop();
+		}
+	}
+
+	cout << res;
+
+	return 0;
+}
