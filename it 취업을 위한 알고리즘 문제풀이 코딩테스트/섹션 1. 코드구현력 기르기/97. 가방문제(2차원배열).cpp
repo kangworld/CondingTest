cfddd341dad03c4de54b9f2e0049e163cf6ce17d
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-10-07 19:53:55 +0900

    Complete
    97. 가방문제(2차원배열)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/97. 가방문제(2차원배열).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, t;
+int w[31], v[31];
+int dp[31][1001];
+
+int main(void)
+{
+	cin >> n >> t;
+
+	for (int i = 1; i <= n; i++)
+		cin >> w[i] >> v[i];
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= t; j++)
+		{
+			if (j - w[i] >= 0)
+			{
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - w[i]] + v[i]);
+			}
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
+
+	cout << dp[n][t];
+
+	return 0;
+}
