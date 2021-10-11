fefdc0aa1334e321daba274e6f4e7789bec26b8f
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-10-11 16:36:36 +0900

    Complete
    76. 이항계수(메모이제이션)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/76. 이항계수(메모이제이션).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int mem[21][21];
+
+int DP(int n, int r)
+{
+	if (n == r || r == 0)
+		return 1;
+
+	if (mem[n][r])
+		return mem[n][r];
+
+	return mem[n][r] = DP(n - 1, r - 1) + DP(n - 1, r);
+}
+
+int main(void)
+{
+	int n, r, res;
+
+	cin >> n >> r;
+
+	res = DP(n, r);
+
+	cout << res;
+
+	return 0;
+}
