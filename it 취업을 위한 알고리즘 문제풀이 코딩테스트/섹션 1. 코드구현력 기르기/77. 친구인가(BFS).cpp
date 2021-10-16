34f02d584d68aff44b3a14b376b00157c422e0a6
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-10-16 20:34:57 +0900

    Complete
    77. 친구인가(BFS)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/77. 친구인가(BFS).cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m, f1, f2, start, dest;
+vector<int> vec[1001];
+vector<bool> visited(1001, false);
+vector<int> path(1001, 0);
+
+void BFS()
+{
+	queue<int> q;
+
+	q.push(start);
+	visited[start] = true;
+	path[start] = start;
+
+	while (!q.empty())
+	{
+		int now = q.front();
+		q.pop();
+
+		for (int next : vec[now])
+		{
+			if (visited[next])
+				continue;
+
+			q.push(next);
+			visited[next] = true;
+			path[next] = now;
+		}
+	}
+}
+
+bool CalculatePath()
+{
+	int now = dest;
+
+	while (now != path[now])
+	{
+		now = path[now];
+		if (now == start)
+			return true;
+	}
+
+	return now == dest ? true : false;
+}
+
+int main(void)
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> f1 >> f2;
+
+		vec[f1].push_back(f2);
+		vec[f2].push_back(f1);
+	}
+	
+	cin >> start >> dest;
+
+	BFS();
+
+	if (CalculatePath())
+		cout << "YES";
+	else
+		cout << "NO";
+}
