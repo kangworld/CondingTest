eaf4ede8ffe39d6adc9c108b4cda73fb1a7321fb
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-10-21 23:46:37 +0900

    Complete
    77. 친구인가(Union&Find 자료구조)
    
    78. 원더랜드(Kruskal MST 알고리즘 우선순위큐)
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/77. 친구인가(Union&Find 자료구조).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m, d1, d2, start, dest;
+int uni[1001];
+
+int Find(int idx)
+{
+	if (uni[idx] == idx)
+		return idx;
+	else
+		return Find(uni[idx]);
+}
+
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	if (a != b)
+		uni[a] = b;
+}
+
+int main(void)
+{
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		uni[i] = i;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> d1 >> d2;
+		Union(d1, d2);
+	}
+
+	cin >> start >> dest;
+
+	if (Find(start) == Find(dest))
+		cout << "YES";
+	else
+		cout << "NO";
+}
---
+++ it 취업을 위한 알고리즘 문제풀이 코딩테스트/섹션 1. 코드구현력 기르기/78. (MARK)원더랜드(Kruskal MST).cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct node
+{
+	int start;
+	int dest;
+	int cost;
+
+	node(int start, int dest, int cost)
+	{
+		this->start = start;
+		this->dest = dest;
+		this->cost = cost;
+	}
+
+	bool operator<(const node &a)
+	{
+		return cost < a.cost;
+	}
+};
+
+int uni[26];
+vector<node> nd;
+int n, m, start, dest, cost, res = 0;
+
+int Find(int a)
+{
+	if (a == uni[a])
+		return a;
+	else
+		return Find(uni[a]);
+}
+
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	if (a != b) uni[a] = b;
+}
+
+bool IsUnion(int a, int b)
+{
+	if (Find(a) == Find(b))
+		return true;
+	else
+		return false;
+}
+
+int main(void)
+{
+	queue<int> path;
+	cin >> n >> m;
+	
+	for (int i = 1; i <= n; i++)
+		uni[i] = i;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> start >> dest >> cost;
+		nd.push_back(node(start, dest, cost));
+	}
+
+	sort(nd.begin(), nd.end());
+
+	for(int i = 0; i < m; i++)
+	{
+		if (!IsUnion(nd[i].start, nd[i].dest))
+		{
+			res += nd[i].cost;
+			Union(nd[i].start, nd[i].dest);
+		}
+	}
+
+	cout << res;
+}
