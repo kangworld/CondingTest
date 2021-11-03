57392831e26c21f4762a2e1971ed2672cd5f73f8
beat6744@nate.com <beat6744@nate.com>
2021-11-04 00:00:45 +0900

    Complete
    두 개 뽑아서 더하기
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ 프로그래머스/Level 1/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> answer;
+vector<int> numbers;
+
+void DFS(int now, int pos, int sum)
+{
+    if (now == 2)
+    {
+        answer.push_back(sum);
+        return;
+    }
+
+    for (int i = pos; i < numbers.size(); i++)
+        DFS(now + 1, i + 1, sum + numbers[i]);
+}
+
+vector<int> solution(vector<int> _numbers)
+{
+    numbers = _numbers;
+
+    DFS(0, 0, 0);
+
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+
+    return answer;
+}
