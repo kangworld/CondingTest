13c7e7d0a9b6a0b1a07a824ef92ae7a53e554bcb
DESKTOP-VEDQL24\KangMinGu <beat6744@nate.com>
2021-11-06 21:31:23 +0900

    Complete
    최소직사각형
    
    (*MARK : 다시 풀어볼 만한 문제)

---
+++ 프로그래머스/Level 1/최소직사각형.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes)
+{
+    int w = 0, h = 0;
+
+    for (int i = 0; i < sizes.size(); i++)
+    {
+        w = max(w, max(sizes[i][0], sizes[i][1]));
+        h = max(h, min(sizes[i][0], sizes[i][1]));
+    }
+
+    return w * h;
+}
